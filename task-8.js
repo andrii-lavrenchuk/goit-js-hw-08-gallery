dd39a42888b444021066e1b91d7113befdd4c1eb
Andrii Lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-09-23 19:02:59 +0300

    Adds final result

--- task-8.js
+++ task-8.js
@@ -1,8 +1,16 @@
 import galleryItems from './gallery-items.js';
 
 const galleryList = document.querySelector('.js-gallery');
+const modal = document.querySelector('.js-lightbox');
+const bigImg = document.querySelector('.lightbox__image');
+const modalCloseBtn = document.querySelector(
+  'button[data-action="close-lightbox"]',
+);
+
 const galleryMarkup = createGalleryItems(galleryItems);
 galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryList.addEventListener('click', openModal);
+modalCloseBtn.addEventListener('click', closeModal);
 
 function createGalleryItems(galleryItems) {
   const markup = galleryItems
@@ -22,4 +30,21 @@ function createGalleryItems(galleryItems) {
     })
     .join('');
   return markup;
+}
+
+function openModal(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  modal.classList.add('is-open');
+  bigImg.src = evt.target.dataset.source;
+  bigImg.alt = evt.target.alt;
+}
+
+function closeModal(evt) {
+  console.log('Закриваю модалку');
+  modal.classList.remove('is-open');
+  bigImg.src = '';
+  bigImg.alt = '';
 }
